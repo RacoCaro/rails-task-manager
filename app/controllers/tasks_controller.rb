2957ce29cb815ad242ab8266bba10004a7be8422
ACaroline Luttringer <thisisraco@gmail.com>
2020-11-11 10:34:05 +0100

    yesterday's exxxo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,20 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params[:task])
+    @task.save
+    redirect_to task_path
+  end
+
 end
